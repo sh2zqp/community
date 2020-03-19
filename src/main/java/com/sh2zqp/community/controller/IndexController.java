47d8aa87ffcee1c0548e40514a3dab253f6f1907
sh2zqp <sh2zqp@gmail.com>
2020-03-19 10:43:44 +0800

    记住用户的登录状态，根据数据库里面user.token信息

--- src/main/java/com/sh2zqp/community/controller/IndexController.java
+++ src/main/java/com/sh2zqp/community/controller/IndexController.java
@@ -1,13 +1,37 @@
 package com.sh2zqp.community.controller;
 
+import com.sh2zqp.community.mapper.UserMapper;
+import com.sh2zqp.community.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @Controller
 public class IndexController {
 
+    @Autowired
+    private UserMapper userMapper;
+
     @GetMapping("/")
-    public String index() {
+    public String index(HttpServletRequest request,
+                        HttpServletResponse response) {
+        Cookie[] cookies = request.getCookies();
+        if (cookies != null && cookies.length != 0) {
+            for (Cookie cookie : cookies) {
+                if (cookie.getName().equals("token")) {
+                    String tokenValue = cookie.getValue();
+                    User user = userMapper.findUserByToken(tokenValue);
+                    if (user != null) {
+                        request.getSession().setAttribute("user", user);
+                    }
+                    break;
+                }
+            }
+        }
         return "index";
     }
 }
